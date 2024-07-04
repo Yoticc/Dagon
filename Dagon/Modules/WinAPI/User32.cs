1a5b86c8d0ed42f5c493d896a9df608abc6bad9d
Yotic <yoticcitoy@gmail.com>
2024-07-04 05:26:58 +0300

    Added a simple window creating API using WinAPI. Setuped Korn for Test.csproj

---
+++ Dagon/Modules/WinAPI/User32.cs
@@ -0,0 +1,39 @@
+﻿namespace Dagon.WinAPI;
+public unsafe class User32
+{
+    public static pointer CreateWindow(
+        WindowStylesEx styleEx,
+        string className,
+        string windowName,
+        WindowStyles style,
+        int x,
+        int y,
+        int width,
+        int height,
+        pointer hWndParent,
+        pointer menu,
+        pointer hInstance,
+        pointer lpParam)
+    {
+        var result = user32.CreateWindowEx((int)styleEx, className, windowName, (int)style, x, y, width, height, hWndParent, menu, hInstance, lpParam);
+        return result;
+    }
+
+    public static pointer CreateWindow(
+        WindowStylesEx styleEx, 
+        ushort className, 
+        string windowName, 
+        WindowStyles style, 
+        int x, 
+        int y, 
+        int width, 
+        int height, 
+        pointer hWndParent, 
+        pointer menu, 
+        pointer hInstance, 
+        pointer lpParam)
+    {
+        var result = user32.CreateWindowEx((int)styleEx, (char*)className, windowName, (int)style, x, y, width, height, hWndParent, menu, hInstance, lpParam);
+        return result;
+    }
+}
--- Test/Program.cs
+++ Test/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using Dagon.WinAPI;
+
+unsafe class Program
+{
+    static void Main()
+    {
+        var hInstance = kernel32.GetModuleHandle((char*)0);
+
+        var window = User32.CreateWindow(0, 32770, "TestApp window", WindowStyles.VISIBLE, 0, 0, 400, 400, 0, 0, hInstance, 0);
+
+        user32.MessageBox(0, $"hInstance: {hInstance}, window: {window}, error: {kernel32.GetLastError()}", "", 0); // You need to stay in the thread
+    }
+}
